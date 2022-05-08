c7c9f579c3f2d6b115cfeb0c3da5220576664335
Diogo Carvalho <dcar84@gmail.com>
2022-05-08 11:05:08 +0000

    add an alert for the user in case they don't enter a username/answer

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -72,63 +72,66 @@ document.getElementById("start_game_button").addEventListener("keydown", functio
  */
 function createPlayer() {
     
-        username = document.getElementById("username").value;
+        if(username = document.getElementById("username").value === "") {
+            alert("Please enter a username before continuing");
+        } else {
+            username = document.getElementById("username").value;
 
-        let html = `<!DOCTYPE html>
-                    <html>
-                    <head>
-                        <meta charset='utf-8'>
-                        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-                        <title>Euro Capitals Quiz!</title>
-                        <meta name='viewport' content='width=device-width, initial-scale=1'>
-                        <link rel="preconnect" href="https://fonts.googleapis.com">
-                        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Rajdhani:wght@300;400&display=swap" rel="stylesheet"> 
-                        <link rel='stylesheet' type='text/css' media='screen' href='assets/css/style.css'>
-                        
-                    </head>
-                    <body>
-                        
-                        <header>
-                                <h1>Euro Capitals Quiz!</h1>
-                                <p>${username}, can you name all the capital cities?</p>                
-                        </header>
+            let html = `<!DOCTYPE html>
+                        <html>
+                        <head>
+                            <meta charset='utf-8'>
+                            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                            <title>Euro Capitals Quiz!</title>
+                            <meta name='viewport' content='width=device-width, initial-scale=1'>
+                            <link rel="preconnect" href="https://fonts.googleapis.com">
+                            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                            <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Rajdhani:wght@300;400&display=swap" rel="stylesheet"> 
+                            <link rel='stylesheet' type='text/css' media='screen' href='assets/css/style.css'>
+                            
+                        </head>
+                        <body>
+                            
+                            <header>
+                                    <h1>Euro Capitals Quiz!</h1>
+                                    <p>${username}, can you name all the capital cities?</p>                
+                            </header>
 
-                        <section id="game_section">
-                            <div id="scoreboard">
-                                <p id="correct_score" class="scoreboard_item">Correct <span id="correct_score_value">0</span></p>
-                                <p id="incorrect_score" class="scoreboard_item">Incorrect <span id="incorrect_score_value">0</span></p>
-                                <p id="questions_remaining" class="scoreboard_item">Remaining <span id="questions_remaining_value">0</span></p>
-                            </div>
-                            <div>
-                                <div id="country" class="question_area">
-                                    <h2 id="country_name">Country: <span id="current_country">Albania</span></h2>
-                                    <img id="country_flag" src="./assets/images/country_flags/Albania Flag.png" alt="the flag of the country that the user must guess the capital city of">
+                            <section id="game_section">
+                                <div id="scoreboard">
+                                    <p id="correct_score" class="scoreboard_item">Correct <span id="correct_score_value">0</span></p>
+                                    <p id="incorrect_score" class="scoreboard_item">Incorrect <span id="incorrect_score_value">0</span></p>
+                                    <p id="questions_remaining" class="scoreboard_item">Remaining <span id="questions_remaining_value">0</span></p>
                                 </div>
-                                <div id="capital" class="question_area">
-                                    <h2>Capital</h2>
-                                    <div id="city_input">
-                                        <label class="city_display" id="city_label" for="capital_city">City:</label>
-                                        <input class="city_display" type="text" id="capital_city" name="capital_city" required>
+                                <div>
+                                    <div id="country" class="question_area">
+                                        <h2 id="country_name">Country: <span id="current_country">Albania</span></h2>
+                                        <img id="country_flag" src="./assets/images/country_flags/Albania Flag.png" alt="the flag of the country that the user must guess the capital city of">
                                     </div>
-                                    
-                                    <button class="city_display game_button" id="answer_button">Submit Answer</button>
+                                    <div id="capital" class="question_area">
+                                        <h2>Capital</h2>
+                                        <div id="city_input">
+                                            <label class="city_display" id="city_label" for="capital_city">City:</label>
+                                            <input class="city_display" type="text" id="capital_city" name="capital_city" required>
+                                        </div>
+                                        
+                                        <button class="city_display game_button" id="answer_button">Submit Answer</button>
+                                    </div>
                                 </div>
-                            </div>
-                            <div>
-                                <button id="new_game_button" class="game_button finish_button">New Game</button>
-                                <button id="quit_game_button" class="game_button finish_button">Quit Game</button>
-                            </div>
-                        </section>
+                                <div>
+                                    <button id="new_game_button" class="game_button finish_button">New Game</button>
+                                    <button id="quit_game_button" class="game_button finish_button">Quit Game</button>
+                                </div>
+                            </section>
 
-                        <script src='assets/JS/script.js'></script>
-                    </body>
-                    </html>`;
+                            <script src='assets/JS/script.js'></script>
+                        </body>
+                        </html>`;
 
-        document.body.innerHTML = html;
-        newGame();
-        document.getElementById("capital_city").value = "";
-        document.getElementById("capital_city").focus();
+            document.body.innerHTML = html;
+            newGame();
+            document.getElementById("capital_city").value = "";
+            document.getElementById("capital_city").focus();
 
         /* adds event listener to submit button to check user response
          also adds key press listener in case user hits the "enter" key instead of clicking the submit button */
@@ -142,6 +145,7 @@ function createPlayer() {
                 checkAnswer();
             }
         });
+    }
 };
 
 /**
@@ -216,7 +220,10 @@ function decrementRemainingQuestions() {
 function checkAnswer() {
     let country = document.getElementById("current_country").textContent;
     let answer = document.getElementById("capital_city").value;
-    if(answer === capitals[country]) {
+    if(answer === "") {
+        alert("Please enter an answer");
+        document.getElementById("capital_city").focus();
+    } else if(answer === capitals[country]) {
         correctScore();
         alert("Good job! Your answer was correct");
         delete capitals[country];
